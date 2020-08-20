74418d91c60c9480f8453eb55b4b3b863a21b491
LeonardoGoPe <leonardo.gomez.1297@gmail.com>
2020-08-20 18:35:12 -0500

    Mapa 1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core';
-declare var google: any;
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  map: any;
-  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+export class HomePage implements OnInit {
+
+  map = null; 
 
   constructor() {}
-  ionViewDidEnter(){
-    this.showMap();
+
+  ngOnInit(){
+    this.loadMap()
   }
-  showMap(){
-    const location= new google.maps.LatLng(-2.1481458,-79.9644885);
-    const options= { center: location, zoom:20 , disableDefaultUI: true}
-    this.map = new google.maps.Map(this.mapRef.nativeElement, options)
+
+  loadMap() {
+    //Crear nuevo mapa
+    const mapEle: HTMLElement = document.getElementById('map');
+    // Crear el mapa y renderizarlo
+    this.map = new google.maps.Map(mapEle, {
+      center: {lat: -2.189327, lng: -79.889532},
+      zoom: 15
+    });
   }
 }

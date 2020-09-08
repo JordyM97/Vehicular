9582f0e6092be81696624efb1d5e7e4b323f0739
Jordy Medina <medinajordy@hotmail.com>
2020-09-07 22:15:37 -0500

    asD

--- src/app/pages/servicio/servicio.module.ts
+++ src/app/pages/servicio/servicio.module.ts
@@ -7,20 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { ServicioPageRoutingModule } from './servicio-routing.module';
 
 import { ServicioPage } from './servicio.page';
-import { ServicioModalPage } from '../servicio-modal/servicio-modal.page';
-import { ServicioModalPageRoutingModule } from '../servicio-modal/servicio-modal-routing.module';
-import { ServicioModalPageModule } from '../servicio-modal/servicio-modal.module';
+
 
 @NgModule({
   entryComponents: [
-    ServicioModalPage //Modal que quiero abrir
   ],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ServicioPageRoutingModule,
-    ServicioModalPageModule //Cargar configuraciones y opciones
+    ServicioPageRoutingModule
   ],
   declarations: [ServicioPage]
 })

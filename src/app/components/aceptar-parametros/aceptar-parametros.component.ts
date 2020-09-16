adc40e67cc1bf3e63b74ba3125d34592619e28ff
LeonardoGoPe <leonardo.gomez.1297@gmail.com>
2020-09-16 17:17:43 -0500

    Algunas correciones

---
+++ src/app/components/aceptar-parametros/aceptar-parametros.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController,ToastController,NavParams } from '@ionic/angular';
+
+@Component({
+  selector: 'app-aceptar-parametros',
+  templateUrl: './aceptar-parametros.component.html',
+  styleUrls: ['./aceptar-parametros.component.scss'],
+})
+export class AceptarParametrosComponent implements OnInit {
+  startMarker: any;
+  constructor(private navParams: NavParams, private popoverController: PopoverController,public toastController: ToastController) { }
+
+  ngOnInit() {
+    this.startMarker= this.navParams.get('info');
+    console.log(this.startMarker);
+  }
+  async DismissClick() {
+    await this.popoverController.dismiss();
+    const toast = await this.toastController.create({
+      message: '¿Vas cambiar algo?',
+      duration: 2500,
+      position: 'top',
+      color: 'danger'
+      });
+    toast.present();
+  }
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Buscando conductor',
+      duration: 25000,
+      position: 'middle',
+      color: 'success'
+      });
+    toast.present();
+    await this.popoverController.dismiss();
+    }
+}
